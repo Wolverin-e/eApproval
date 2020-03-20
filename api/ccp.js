68c069183082e0889f8b79e25e9a5c6909b4fc18
Wolverin-e <patel.6@iitj.ac.in>
2020-03-20 16:45:07 +0530

    some necessary removals.

--- api/ccp.js
+++ api/ccp.js
@@ -2,9 +2,12 @@ const fs = require("fs");
 const path = require("path");
 
 const getCCP = org => {
-    const ccpPath = path.resolve(__dirname, "..", "configs", "ccp", `connection-${org}.json`);
-    const ccp = JSON.parse(fs.readFileSync(ccpPath));
+    const ccpPath = path.resolve(__dirname, "..", "configs",
+                                     "ccp", `connection-${org}.json`);
+    const ccp = JSON.parse(fs.readFileSync(ccpPath, "utf8"));
     return ccp
 }
+
+// console.log(getCCP("org1"));
 
 module.exports = getCCP;
