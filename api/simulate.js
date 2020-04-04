35a3167568b1c4996baf0de33933a4f450f2466c
dhruv0000 <patel.4@iitj.ac.in>
2020-04-04 08:11:31 +0530

    Added CORS support to simulation API

--- api/simulate.js
+++ api/simulate.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.get("/api/pendingRequests", (req, res) => {
     res.send({
