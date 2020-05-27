31d0a1ab9638cb3511121245c513da9af8da3533
Wolverin-e <patel.6@iitj.ac.in>
2020-05-27 18:06:54 +0530

    some changes.

--- api/registerUser.js
+++ api/registerUser.js
@@ -2,10 +2,6 @@ const register = require('./utils/register');
 const sqlite3 = require('sqlite3').verbose();
 let User = require('./utils/User');
 
-let db = new sqlite3.Database('./users.db', sqlite3.OPEN_READWRITE, (err) =>{
-    if(err) console.error(err);
-})
-
 User.dbname = './users.db'
 let user = new User(UserName=process.env.USR, Password="123")
 user.saveToDB().then(() => {
