0b28fcf42506860ff428571d7d17d3fd58fbfee6
dhruv0000 <patel.4@iitj.ac.in>
2020-05-31 04:43:10 +0530

    Added PvtRemarks field for orgs

--- ui/client/src/components/pendingRequests.js
+++ ui/client/src/components/pendingRequests.js
@@ -7,23 +7,33 @@ class PendingRequest extends React.Component {
     super(props)
     this.state = {
       data: [],
-      // request = {
-      //   req_key: '',
-      //   department: '',
-      //   remarks: ''
-      // }
+      request: {
+        req_key: 'PENDING',
+        department: this.props.department,
+        remarks: '',
+        pvtRemarks: {
+          text:''
+        },
+      }
     }
     this.handleAccept = this.handleAccept.bind(this)
     this.handleDecline = this.handleDecline.bind(this)
+    this.onChange = this.onChange.bind(this)
   }
 
-  handleAccept() {
-    let request = {
-      req_key: 'PENDING',
-      department: this.props.department,
-      remarks: 'GOOD!'
-      // Remarks to be added
+  onChange(e) {
+    if (e.target.id === 'Public_Remarks') {
+        this.setState({ request: {...this.state.request, remarks: e.target.value.toString()} });
+    } else if (e.target.id === 'Private_Request_text') {
+        this.setState({ request: {...this.state.request, pvtRemarks: {...this.state.request.pvtRemarks,text:e.target.value.toString() }} });
     }
+    console.log(this.state.request.pvtRemarks);
+    }
+
+  handleAccept() {
+
+    let request = this.state.request
+    console.log(request)
     fetch('http://127.0.0.1:8000/api/approve', {
         method: 'POST',
         body: JSON.stringify(request),
@@ -33,8 +43,8 @@ class PendingRequest extends React.Component {
     }).then(response => {
         if (response.status >= 200 && response.status < 300) {
             console.log(response);
+            window.location.reload();
             return response;
-            // window.location.reload();
           } else {
            console.log('Somthing happened wrong');
           }
@@ -42,12 +52,7 @@ class PendingRequest extends React.Component {
   }
 
   handleDecline() {
-    let request = {
-      req_key: 'PENDING',
-      department: this.props.department,
-      remarks: 'HARMFUL!'
-      // Remarks to be added
-    }
+    let request = this.state.request
     console.log(request)
     fetch('http://127.0.0.1:8000/api/decline', {
         method: 'POST',
@@ -91,16 +96,22 @@ class PendingRequest extends React.Component {
       <div>
         <h1>{this.props.department}</h1>
         <h2>Pending Requests</h2>
-        <table class="rwd-table">
-          <tr>
-            <th>Title</th>
-            <th>Description</th>
-            <th>ORG1</th>
-            <th>ORG2</th>
-            <th>Request By</th>
-            <th>Action</th>
-          </tr>
-          {this.state.data.map(arg => <PendingRequestObj Val = {arg.Val} handleAccept={this.handleAccept} handleDecline={this.handleDecline}/>)}
+        <table className="rwd-table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Description</th>
+              <th>ORG1</th>
+              <th>ORG2</th>
+              <th>Request By</th>
+              <th>Public Remarks</th>
+              <th>Private Remarks</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.data.map(arg => <PendingRequestObj Val = {arg.Val} onChange={this.onChange} handleAccept={this.handleAccept} handleDecline={this.handleDecline}/>)}
+          </tbody>
     </table>
     </div>
     )
@@ -117,7 +128,9 @@ const PendingRequestObj = (props) => {
     <td data-th="ORG1">{props.Val.approvals.ORG1.status}</td>
     <td data-th="ORG2">{props.Val.approvals.ORG1.status}</td>
     <td data-th="Request By">{props.Val.from_user}</td>
-    <td><button onClick={props.handleAccept}>Approve</button><button onClick={props.handleDecline}>Decline</button></td>
+    <td data-th="Public Remarks"><input type="textfield" id="Public_Remarks" name="Public Remarks_text" onChange={props.onChange} /></td>
+    <td data-th="Private Remarks"><input type="textfield" id="Private_Request_text" name="Private_Remarks_text" onChange={props.onChange} /></td>
+    <td><button id="Approve" onClick={props.handleAccept}>Approve</button><button id="Decline" onClick={props.handleDecline}>Decline</button></td>
     </tr>
   </React.Fragment>
   )
